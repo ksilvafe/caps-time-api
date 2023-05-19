21e7a8a01e4f9ab7a0168de6d5ed17e9039232db
C1325301 Kevin da Silva Ferreira <C1325301@interno.bb.com.br>
2023-05-18 23:31:18 -0300

    feat: create model and routes memory

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
-
+import { memoriesRoutes } from './routes/memories'
 const server = fastify()
+
+server.register(memoriesRoutes)
+
+server.register(cors, {
+  origin: true,
+})
 
 server.listen({ port: 3333 }).then(() => {
   console.log('Server listening on port 3000')
